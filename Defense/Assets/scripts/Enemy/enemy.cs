d85ee410d77ebf35736735da541b06fd50808752
IgnacioIngold <43892862+IgnacioIngold@users.noreply.github.com>
2020-09-25 17:21:21 -0300

    pequeño enemigo

---
+++ Defense/Assets/scripts/Enemy/enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy : MonoBehaviour
+{
+    #region variables publicas
+    public Transform target;
+    public float speed;
+    #endregion
+    Vector3 dir;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dir = (target.transform.position - transform.position).normalized;
+        transform.position += dir * speed * Time.deltaTime;
+    }
+}
